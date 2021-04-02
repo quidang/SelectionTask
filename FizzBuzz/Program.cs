5c1892157363aba27411f751458210622a909825
Qui <103585238@student.swin.edu.au>
2021-04-02 16:29:21 +1100

    Added pseudocode and changed fizzbuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,6 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine(17 % 5);
             for (int i = 1; i <= 100; i++) {
                 if (i % 3 == 0 && i % 5 == 0) {
                     Console.WriteLine(String.Format("FizzBuzz: {0}", i));
