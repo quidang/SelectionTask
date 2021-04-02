fd67ebf44137c2f0760480a128ab418adc5ca689
Qui <103585238@student.swin.edu.au>
2021-04-02 16:00:43 +1100

    Initial commit cleaned up repo

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine(17 % 5);
+            for (int i = 1; i <= 100; i++) {
+                if (i % 3 == 0 && i % 5 == 0) {
+                    Console.WriteLine(String.Format("FizzBuzz: {0}", i));
+                }
+                else if (i % 3 == 0) {
+                    Console.WriteLine(String.Format("Fizz: {0}", i));    
+                }
+                else if (i % 5 == 0) {
+                    Console.WriteLine(String.Format("Buzz: {0}", i));
+                }
+            }
+        }
+    }
+}
---
+++ SelectionTask/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // Ask for persons age
+            Console.WriteLine("Please enter your age!");
+            int age = int.Parse(Console.ReadLine());
+
+            // If they're greater than 17 they are of legal adult age! 
+            if (age >= 17 ) {
+            Console.WriteLine("You are of legal adult age!");
+            } else {
+            Console.WriteLine("You are not of legal adult age!"); 
+            }
+
+            // Ask for persons gender
+            Console.WriteLine("What is your gender?");
+            string gender = Console.ReadLine();
+            Console.WriteLine("This person is " + gender + " and " + age +  " years old ");
+            
+            // Asking for two numbers
+            Console.WriteLine("Input a number");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input a second number");
+            int number2 = int.Parse(Console.ReadLine()); 
+            if (number1 + number2 >= 10) {
+                Console.WriteLine("Sum of numbers is greater than 10");
+            }
+            else {
+                Console.WriteLine("Sum of numbers is less than 10");
+            }
+            
+            // Exercise 2 - Username and Password
+            string username = "Qui";
+            string password = "Dang";
+
+            
+            Console.WriteLine("Enter your username!");
+            string inputUsername = Console.ReadLine();
+
+            if(inputUsername == username) {
+                Console.WriteLine("Enter your password!");
+                string inputPassword = Console.ReadLine();
+                
+                 if (inputPassword == password) {
+                    Console.WriteLine("Login successful!");
+                } else {
+                    Console.WriteLine("Login unsuccessful!");
+                }
+
+            } else {
+                Console.WriteLine("Login unsuccessful!");
+            }
+        }
+    }
+} 
