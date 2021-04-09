82ca8022e42dddce224d488504738c12e3806d09
Qui <103585238@student.swin.edu.au>
2021-04-09 18:48:48 +1000

    nestedrepetitiontask

---
+++ NestedRepetition/NestedRepetitionTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NestedRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // printing out their name 10 times.
+            // Console.WriteLine("Please enter your first name!");
+            // string name = (Console.ReadLine());
+
+            //    for (int i = 0; i < 10; i++) { 
+            //     Console.WriteLine(name);
+            //   }
+            // user inputting their own number for their name to be printed.
+            Console.WriteLine("Please enter your first name!");
+            string name = (Console.ReadLine());
+
+            Console.WriteLine("Enter the amount of times you want your first name to be printed"); 
+            int number = int.Parse(Console.ReadLine());
+                    
+                for (int i = 0; i < number; i++) {
+                    Console.WriteLine(name);
+                }
+                    
+        }
+    }   
+}
