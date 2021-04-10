03161c488e2e2572fc581291d36f1e470f6697a5
Qui <103585238@student.swin.edu.au>
2021-04-10 15:14:39 +1000

    finished nested repetition task

--- NestedRepetition/NestedRepetitionTask/Program.cs
+++ NestedRepetition/NestedRepetitionTask/Program.cs
@@ -7,23 +7,35 @@ namespace NestedRepetition
         static void Main(string[] args)
         {
             // printing out their name 10 times.
-            // Console.WriteLine("Please enter your first name!");
-            // string name = (Console.ReadLine());
-
-            //    for (int i = 0; i < 10; i++) { 
-            //     Console.WriteLine(name);
-            //   }
-            // user inputting their own number for their name to be printed.
+            Console.WriteLine("Exericse 1, Task 1");
             Console.WriteLine("Please enter your first name!");
-            string name = (Console.ReadLine());
+            string firstname = (Console.ReadLine());
 
-            Console.WriteLine("Enter the amount of times you want your first name to be printed"); 
+               for (int i = 0; i < 10; i++) { 
+                Console.WriteLine(firstname);
+               }
+            // user inputting their own number for their name to be printed.
+            Console.WriteLine("Exercise 1, Task 2");
+            Console.WriteLine("Enter the amount of times you want your first name to be printed");
             int number = int.Parse(Console.ReadLine());
-                    
-                for (int i = 0; i < number; i++) {
-                    Console.WriteLine(name);
-                }
-                    
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(firstname);
+            }
+
+            string result;
+
+            Console.WriteLine("Exercise 2, Task 1");
+            for (int i = 1; i <= 5; i++) {
+                result = new String('x', i);
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Exercuse 2, Task 2");
+            for (int i = 5; i >= 1; i--) {
+                result = new String('x', i);
+                Console.WriteLine(result);
+            }
         }
-    }   
+    }
 }
